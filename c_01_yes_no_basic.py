1a0967c65fb378b4abb561a8863ef2e6f6e8c742
bighollow <patersonj3132@masseyhigh.school.nz>
2025-02-05 10:05:02 +1300

    simple yes no cheaker (incomplete)

---
+++ c_01_yes_no_basic.py
@@ -0,0 +1,12 @@
+want_instructions =input("do you want to see the instructions? ").lower()
+#
+#  #user says yes/no
+if want_instructions == "yes" or want_instructions == "y":
+    print("you said yes")
+elif want_instructions == "no" or want_instructions == "n":
+    print("you said no")
+else:
+    print("please enter yes or no")
+
+
+print("hello")
