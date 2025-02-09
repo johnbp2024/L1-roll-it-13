e133d2d03c4c2b004cc6e6f8c7a9e3b39238855e
bighollow <patersonj3132@masseyhigh.school.nz>
2025-02-10 10:17:47 +1300

    looping yes/ no cheaker

--- c_01_yes_no_basic.py
+++ c_01_yes_no_basic.py
@@ -1,12 +1,17 @@
-want_instructions =input("do you want to see the instructions? ").lower()
-#
-#  #user says yes/no
-if want_instructions == "yes" or want_instructions == "y":
-    print("you said yes")
-elif want_instructions == "no" or want_instructions == "n":
-    print("you said no")
-else:
-    print("please enter yes or no")
 
 
-print("hello")
+while True:
+    want_instructions = input("do you want to see the instructions? ").lower()
+    #  #user says yes/no
+    if want_instructions == "yes" or want_instructions == "y":
+        print("you said yes")
+        break
+    elif want_instructions == "no" or want_instructions == "n":
+        print("you said no")
+        break
+    else:
+        print("please enter yes or no")
+        continue
+
+
+print("we done")
