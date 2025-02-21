834e0262140f0a0bef789609d7332a840f6b189e
bighollow <patersonj3132@masseyhigh.school.nz>
2025-02-21 14:08:29 +1300

    working single rounds

--- c_01_one_round_v2.py
+++ c_01_one_round_v2.py
@@ -1,5 +1,6 @@
 import random
 
+
 def inital_points(which_player):
     double = "no"
 
@@ -13,7 +14,13 @@ def inital_points(which_player):
     print(f"{which_player}    - roll 1: {roll_one} \t| roll 2: {roll_two} \t| total: {total} ")
     return total, double
 
-#main starts here
+def make_statement(statement, decoration):
+    """adds emoji/ additional charictors to the start and end of sentances"""
+
+    ends = decoration * 3
+    print(f"\n{ends} {statement} {ends}")
+
+# main starts here
 
 initial_user = inital_points("user")
 initial_comp = inital_points("comp")
@@ -44,7 +51,7 @@ while player_1_points < 13 and player_2_points < 13:
     print()
     input("please press enter to continue this round\n")
 
-    player_1_roll = random.randint(1,  6)
+    player_1_roll = random.randint(1, 6)
     player_1_points += player_1_roll
 
     print(f"{first}: rolled a {player_1_roll} - has {player_1_points}points")
@@ -52,11 +59,30 @@ while player_1_points < 13 and player_2_points < 13:
     if player_1_points >= 13:
         break
 
-
-    player_2_roll = random.randint(1,  6)
+    player_2_roll = random.randint(1, 6)
     player_2_points += player_2_roll
 
     print(f"{second}: rolled a {player_2_roll} - has {player_2_points}points")
 
     print(f"{first}: {player_1_points}  | {second} {player_2_points}")
-print("end of round")
+# end of round
+user_points = player_1_points
+comp_points = player_2_points
+
+if first == "conputer":
+    user_points, comp_points = comp_points, user_points
+
+if user_points > comp_points:
+    winner = "user"
+else:
+    winner = "computer"
+
+round_feedback = f"the {winner} won"
+
+if winner == "user" and double_user == "yes":
+    user_points = user_points * 2
+
+make_statement("round results", "=")
+print(f"user points: {user_points} | computer points: {comp_points}")
+print(round_feedback)
+print()
